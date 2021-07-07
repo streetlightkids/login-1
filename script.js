bc010a8ebb4a547c5667d15fe134a2c2da7e3c56
streetlightkids <ltj_hq@hotmail.com>
2021-07-06 21:05:31 -0400

    Learning so much stuff
    We went over exactly how the onsubmit and addEventListeners work

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.forms["login"].addEventListener("submit", (e) => e.preventDefault());
 
 function testInput() {
+    console.log('berries')
+    const form = document.forms["login"];
     if (form.checkValidity()) {
         window.location = "success.html";
     }
